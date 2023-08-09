54a86af1dbddfb741fbed4b810cc91f03cd42454
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-08-09 12:10:14 -0500

    New sounds :)

--- js/common/view/OrbitalSound.ts
+++ js/common/view/OrbitalSound.ts
@@ -9,7 +9,8 @@ import keplersLaws from '../../keplersLaws.js';
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
 import soundManager from '../../../../tambo/js/soundManager.js';
 // import saturatedSineLoopTrimmed_wav from '../../../sounds/saturatedSineLoopTrimmed_wav.js';
-import charges_wav from '../../../sounds/charges_wav.js';
+// import charges_wav from '../../../sounds/charges_wav.js';
+import OrbitEccentricity_loop_v2_wav from '../../../sounds/OrbitEccentricity_loop_v2_wav.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Multilink from '../../../../axon/js/Multilink.js';
 import animationFrameTimer from '../../../../axon/js/animationFrameTimer.js';
@@ -22,15 +23,23 @@ export default class OrbitalSound {
   public constructor(
     private readonly semimajorAxisProperty: TReadOnlyProperty<number>,
     private readonly eccentricityProperty: TReadOnlyProperty<number> ) {
-    this.orbitalSoundClip = new SoundClip( charges_wav, {
+    this.orbitalSoundClip = new SoundClip( OrbitEccentricity_loop_v2_wav, {
       initialOutputLevel: 0.5,
       loop: true
     } );
     soundManager.addSoundGenerator( this.orbitalSoundClip );
 
+    const maxPlaybackRate = 4;
+    const minPlaybackRate = 0.1;
+
     Multilink.multilink( [ this.semimajorAxisProperty ], () => {
-      this.orbitalSoundClip.setPlaybackRate( Utils.clamp(
-        Utils.linear( 0, 4, 4, 1, Math.pow( this.semimajorAxisProperty.value, 2 ) ), 0.1, 4 ) );
+      const mapping = Utils.clamp(
+        Utils.linear(
+          minPlaybackRate, maxPlaybackRate * 0.7,
+          maxPlaybackRate, minPlaybackRate, Math.pow( this.semimajorAxisProperty.value, 0.5 ) ),
+        minPlaybackRate, maxPlaybackRate
+      );
+      this.orbitalSoundClip.setPlaybackRate( mapping );
     } );
 
     animationFrameTimer.addListener( () => {
@@ -38,7 +47,7 @@ export default class OrbitalSound {
       this.internalTime %= 1000;
       const e = this.eccentricityProperty.value;
       const baseLevel = 0.1;
-      this.orbitalSoundClip.setOutputLevel( baseLevel + baseLevel * e * Math.sin( 1.5 * this.internalTime / 2 ) );
+      this.orbitalSoundClip.setOutputLevel( baseLevel + 1.5 * baseLevel * e * Math.sin( 1.5 * this.internalTime / 2 ) );
     } );
   }
 
