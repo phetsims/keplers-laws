241bd38b694a610f735c4e45611168ab83a21746
AgustinVallejo <agustin.vallejo@udea.edu.co>
2023-08-16 23:36:07 -0500

    Removing tandem from color, see https://github.com/phetsims/keplers-laws/issues/93

--- js/common/KeplersLawsColors.ts
+++ js/common/KeplersLawsColors.ts
@@ -7,7 +7,6 @@
  */
 
 import { ProfileColorProperty } from '../../../scenery/js/imports.js';
-import Tandem from '../../../tandem/js/Tandem.js';
 import keplersLaws from '../keplersLaws.js';
 
 const KeplersLawsColors = {
@@ -16,43 +15,31 @@ const KeplersLawsColors = {
   foregroundProperty: new ProfileColorProperty( keplersLaws, 'foreground', {
     default: 'white',
     projector: 'black'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'foregroundColorProperty' )
   } ),
 
   // Semimajor axis color
   semimajorAxisColorProperty: new ProfileColorProperty( keplersLaws, 'semimajorAxis', {
     default: 'orange'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'semimajorAxisColorProperty' )
   } ),
 
   // Semiminor axis color
   semiminorAxisColorProperty: new ProfileColorProperty( keplersLaws, 'semiminorAxis', {
     default: '#C4F566'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'semiminorAxisColorProperty' )
   } ),
 
   // Focal distance color
   focalDistanceColorProperty: new ProfileColorProperty( keplersLaws, 'focalDistance', {
     default: '#AFAFDA'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'focalDistanceColorProperty' )
   } ),
 
   // Period color
   periodColorProperty: new ProfileColorProperty( keplersLaws, 'period', {
     default: 'cyan'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'periodColorProperty' )
   } ),
 
   // Period color
   distancesColorProperty: new ProfileColorProperty( keplersLaws, 'distance', {
     default: '#ccb285'
-  }, {
-    tandem: Tandem.COLORS.createTandem( 'distancesColorProperty' )
   } )
 };
 
