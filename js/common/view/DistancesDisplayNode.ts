b53d90bacc4f7d0edb1f1731fc2d11d03baec7c0
pixelzoom <cmalley@pixelzoom.com>
2023-08-29 13:49:34 -0600

    a lot of cleanup, and dynamically center labels on arrows (with some duplicate code still to be addressed), https://github.com/phetsims/keplers-laws/issues/98

--- js/common/view/DistancesDisplayNode.ts
+++ js/common/view/DistancesDisplayNode.ts
@@ -9,7 +9,7 @@
  * @author Agustín Vallejo
  */
 
-import { HBox, Path, RichText, TextOptions, VBox } from '../../../../scenery/js/imports.js';
+import { HBox, Node, Path, RichText, TextOptions, VBox } from '../../../../scenery/js/imports.js';
 import KeplersLawsModel from '../model/KeplersLawsModel.js';
 import EllipticalOrbitEngine from '../model/EllipticalOrbitEngine.js';
 import LineArrowNode from '../../../../scenery-phet/js/LineArrowNode.js';
@@ -28,6 +28,7 @@ import KeplersLawsColors from '../KeplersLawsColors.js';
 const INITIAL_ARROW_LENGTH = 5;
 const SYMBOL_MAX_WIDTH = 20;
 
+// Options for the 'd1' and 'd2' labels
 export const DISTANCE_LABEL_OPTIONS = combineOptions<TextOptions>( {}, KeplersLawsConstants.TEXT_OPTIONS, {
   scale: 1.5,
   stroke: KeplersLawsColors.distancesColorProperty,
@@ -35,14 +36,8 @@ export const DISTANCE_LABEL_OPTIONS = combineOptions<TextOptions>( {}, KeplersLa
   maxWidth: SYMBOL_MAX_WIDTH
 } );
 
-export const AXIS_LABEL_OPTIONS = combineOptions<TextOptions>( {}, KeplersLawsConstants.TEXT_OPTIONS, {
-  scale: 1.5,
-  stroke: KeplersLawsColors.semiMajorAxisColorProperty,
-  fill: KeplersLawsColors.semiMajorAxisColorProperty,
-  maxWidth: SYMBOL_MAX_WIDTH
-} );
-
-export const STRING_ARROW_OPTIONS = {
+// Options for the 'd1' and 'd2' arrows
+export const DISTANCE_ARROW_OPTIONS = {
   stroke: '#ccb285',
   headHeight: 10,
   headWidth: 10,
@@ -52,6 +47,15 @@ export const STRING_ARROW_OPTIONS = {
   lineWidth: 3 // See note below
 };
 
+// Options for the 'a' labels
+export const MAJOR_AXIS_LABEL_OPTIONS = combineOptions<TextOptions>( {}, KeplersLawsConstants.TEXT_OPTIONS, {
+  scale: 1.5,
+  stroke: KeplersLawsColors.semiMajorAxisColorProperty,
+  fill: KeplersLawsColors.semiMajorAxisColorProperty,
+  maxWidth: SYMBOL_MAX_WIDTH
+} );
+
+// Options for the 'a' arrows
 export const MAJOR_AXIS_ARROW_OPTIONS = {
   stroke: KeplersLawsColors.semiMajorAxisColorProperty,
   fill: KeplersLawsColors.semiMajorAxisColorProperty,
@@ -62,9 +66,9 @@ export const MAJOR_AXIS_ARROW_OPTIONS = {
   lineWidth: 3 // See note below
 };
 
-// Note: lineWidth is not supported by LineArrowNode but it is supported by Path.
+// Note: lineWidth is not supported by LineArrowNode, but it is supported by Path.
 // This is a workaround to use the same arrow options in the butts to make it
-// Look like a DimensionalArrowNode (which is not yet implemented in common code)
+// look like a DimensionalArrowNode (which is not yet implemented in common code)
 
 export default class DistancesDisplayNode extends VBox {
   public orbit: EllipticalOrbitEngine;
@@ -81,63 +85,60 @@ export default class DistancesDisplayNode extends VBox {
 
     this.orbit = model.engine;
 
-    const stringLabelNode1 = new RichText( '', DISTANCE_LABEL_OPTIONS );
-    const stringLabelNode2 = new RichText( '', DISTANCE_LABEL_OPTIONS );
-
-    const d1lineArrowNode = new LineArrowNode( 0, 0, 0, 1, STRING_ARROW_OPTIONS );
-    const d2lineArrowNode = new LineArrowNode( 0, 0, 0, 1, STRING_ARROW_OPTIONS );
-
-    d1lineArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), STRING_ARROW_OPTIONS ) );
-    d2lineArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), STRING_ARROW_OPTIONS ) );
-
-    const aLineArrowNode1 = new LineArrowNode( 0, 0, 0, 1, MAJOR_AXIS_ARROW_OPTIONS );
-    const aLineArrowNode2 = new LineArrowNode( 0, 0, 0, 1, MAJOR_AXIS_ARROW_OPTIONS );
-    aLineArrowNode1.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), MAJOR_AXIS_ARROW_OPTIONS ) );
-    aLineArrowNode2.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), MAJOR_AXIS_ARROW_OPTIONS ) );
-
-
-    const focalStringsBox = new HBox( {
-      visibleProperty: model.stringsVisibleProperty,
-      children: [
-        new VBox( {
-          children: [
-            stringLabelNode1,
-            d1lineArrowNode
-          ]
-        } ),
-        new VBox( {
-          children: [
-            stringLabelNode2,
-            d2lineArrowNode
-          ]
-        } )
-      ]
+    //   d1    d2
+    // |----><-----|
+    const d1LabelNode = new RichText( '', DISTANCE_LABEL_OPTIONS );
+    const d2LabelNode = new RichText( '', DISTANCE_LABEL_OPTIONS );
+    const d1ArrowNode = new LineArrowNode( 0, 0, 1, 0, DISTANCE_ARROW_OPTIONS );
+    const d2ArrowNode = new LineArrowNode( 0, 0, 1, 0, DISTANCE_ARROW_OPTIONS );
+    d1ArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), DISTANCE_ARROW_OPTIONS ) );
+    d2ArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), DISTANCE_ARROW_OPTIONS ) );
+    const distanceArrowsNode = new HBox( {
+      children: [ d1ArrowNode, d2ArrowNode ]
+    } );
+    const distanceDisplayNode = new Node( {
+      children: [ distanceArrowsNode, d1LabelNode, d2LabelNode ]
+    } );
+    Multilink.multilink( [ d1LabelNode.boundsProperty, d1ArrowNode.boundsProperty ], () => {
+      d1LabelNode.centerX = d1ArrowNode.centerX;
+      d1LabelNode.bottom = d1ArrowNode.top - 1;
+    } );
+    Multilink.multilink( [ d2LabelNode.boundsProperty, d2ArrowNode.boundsProperty ], () => {
+      d2LabelNode.centerX = d2ArrowNode.centerX;
+      d2LabelNode.bottom = d2ArrowNode.top - 1;
     } );
 
-    const majorAxisBox = new HBox( {
-      visibleProperty: model.semiaxisVisibleProperty,
-      children: [
-        new VBox( {
-          children: [
-            aLineArrowNode1,
-            new RichText( KeplersLawsStrings.symbols.semiMajorAxisStringProperty, AXIS_LABEL_OPTIONS )
-          ]
-        } ),
-        new VBox( {
-          children: [
-            aLineArrowNode2,
-            new RichText( KeplersLawsStrings.symbols.semiMajorAxisStringProperty, AXIS_LABEL_OPTIONS )
-          ]
-        } )
-      ]
+    // |----><-----|
+    //    a    a
+    const a1LabelNode = new RichText( KeplersLawsStrings.symbols.semiMajorAxisStringProperty, MAJOR_AXIS_LABEL_OPTIONS );
+    const a2LabelNode = new RichText( KeplersLawsStrings.symbols.semiMajorAxisStringProperty, MAJOR_AXIS_LABEL_OPTIONS );
+    const a1ArrowNode = new LineArrowNode( 0, 0, 1, 0, MAJOR_AXIS_ARROW_OPTIONS );
+    const a2ArrowNode = new LineArrowNode( 0, 0, 1, 0, MAJOR_AXIS_ARROW_OPTIONS );
+    a1ArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), MAJOR_AXIS_ARROW_OPTIONS ) );
+    a2ArrowNode.addChild( new Path( new Shape().moveTo( 0, -INITIAL_ARROW_LENGTH ).lineTo( 0, INITIAL_ARROW_LENGTH ), MAJOR_AXIS_ARROW_OPTIONS ) );
+    const majorAxisArrowsNode = new HBox( {
+      visibleProperty: model.stringsVisibleProperty,
+      children: [ a1ArrowNode, a2ArrowNode ]
+    } );
+    const majorAxisDisplayNode = new Node( {
+      children: [ majorAxisArrowsNode, a1LabelNode, a2LabelNode ],
+      visibleProperty: model.semiaxisVisibleProperty
+    } );
+    Multilink.multilink( [ a1LabelNode.boundsProperty, a1ArrowNode.boundsProperty ], () => {
+      a1LabelNode.centerX = a1ArrowNode.centerX;
+      a1LabelNode.top = a1ArrowNode.bottom + 1;
+    } );
+    Multilink.multilink( [ a2LabelNode.boundsProperty, a2ArrowNode.boundsProperty ], () => {
+      a2LabelNode.centerX = a2ArrowNode.centerX;
+      a2LabelNode.top = a2ArrowNode.bottom + 1;
     } );
 
     this.children = [
-      focalStringsBox,
-      majorAxisBox
+      distanceDisplayNode,
+      majorAxisDisplayNode
     ];
 
-    const updateDistances = () => {
+    const updateArrowNodes = () => {
       const scale = this.modelViewTransformProperty.value.modelToViewDeltaX( 1 );
       const aLength = this.orbit.a * scale;
       const d1Length = this.orbit.d1 * scale;
@@ -147,19 +148,19 @@ export default class DistancesDisplayNode extends VBox {
       const d1String = KeplersLawsStrings.symbols.distance1StringProperty.value;
       const d2String = KeplersLawsStrings.symbols.distance2StringProperty.value;
 
-      stringLabelNode1.setString( this.orbit.isCircularProperty.value ? rString : d1String );
-      stringLabelNode2.setString( this.orbit.isCircularProperty.value ? rString : d2String );
+      d1LabelNode.setString( this.orbit.isCircularProperty.value ? rString : d1String );
+      d2LabelNode.setString( this.orbit.isCircularProperty.value ? rString : d2String );
 
-      stringLabelNode1.x = -d2Length / 2;
-      stringLabelNode2.x = d1Length / 2;
+      d1LabelNode.x = -d2Length / 2;
+      d2LabelNode.x = d1Length / 2;
 
       const dx = 3; // reducing the arrow length a bit to get the proper measurable length in screen
 
-      d1lineArrowNode.setTailAndTip( 0, 0, d1Length - dx, 0 );
-      d2lineArrowNode.setTailAndTip( 0, 0, -d2Length + dx, 0 );
+      d1ArrowNode.setTailAndTip( 0, 0, d1Length - dx, 0 );
+      d2ArrowNode.setTailAndTip( 0, 0, -d2Length + dx, 0 );
 
-      aLineArrowNode1.setTailAndTip( 0, 0, aLength - dx, 0 );
-      aLineArrowNode2.setTailAndTip( 0, 0, -aLength + dx, 0 );
+      a1ArrowNode.setTailAndTip( 0, 0, aLength - dx, 0 );
+      a2ArrowNode.setTailAndTip( 0, 0, -aLength + dx, 0 );
     };
 
     Multilink.multilink( [
@@ -167,9 +168,9 @@ export default class DistancesDisplayNode extends VBox {
       KeplersLawsStrings.symbols.distance1StringProperty,
       KeplersLawsStrings.symbols.distance2StringProperty,
       model.zoomProperty
-    ], updateDistances );
+    ], updateArrowNodes );
 
-    this.orbit.ranEmitter.addListener( updateDistances );
+    this.orbit.ranEmitter.addListener( updateArrowNodes );
   }
 }
 
