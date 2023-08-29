b51b63f69353f256db27a4672bf0a624b1fbe8c4
pixelzoom <cmalley@pixelzoom.com>
2023-08-29 16:01:27 -0600

    maxWidth for fraction symbols and units, https://github.com/phetsims/keplers-laws/issues/86

--- js/common/view/ThirdLawAccordionBox.ts
+++ js/common/view/ThirdLawAccordionBox.ts
@@ -12,7 +12,7 @@ import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import KeplersLawsStrings from '../../KeplersLawsStrings.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
-import { GridBox, HBox, RichText, Text, TextOptions, VBox } from '../../../../scenery/js/imports.js';
+import { GridBox, HBox, RichText, RichTextOptions, Text, TextOptions, VBox } from '../../../../scenery/js/imports.js';
 import KeplersLawsConstants from '../KeplersLawsConstants.js';
 import SolarSystemCommonColors from '../../../../solar-system-common/js/SolarSystemCommonColors.js';
 import SolarSystemCommonConstants from '../../../../solar-system-common/js/SolarSystemCommonConstants.js';
@@ -34,8 +34,15 @@ const RADIO_BUTTON_TEXT_OPTIONS = {
   maxWidth: 25
 };
 
+const UNITS_TEXT_OPTIONS = {
+  font: new PhetFont( 12 ),
+  fill: SolarSystemCommonColors.foregroundProperty,
+  maxWidth: 35
+};
+
 export default class ThirdLawAccordionBox extends AccordionBox {
   public constructor( model: KeplersLawsModel ) {
+
     const titleStringProperty = new DerivedProperty( [
       KeplersLawsStrings.graph.titleStringProperty,
       model.selectedAxisPowerProperty,
@@ -49,6 +56,7 @@ export default class ThirdLawAccordionBox extends AccordionBox {
         periodPower: periodString
       } );
     } );
+
     const options = combineOptions<ThirdLawAccordionBoxOptions>( {
       titleNode: new RichText( titleStringProperty, KeplersLawsConstants.TITLE_OPTIONS ),
       accessibleName: titleStringProperty,
@@ -66,31 +74,35 @@ export default class ThirdLawAccordionBox extends AccordionBox {
       useExpandedBoundsWhenCollapsed: false
     }, SolarSystemCommonConstants.CONTROL_PANEL_OPTIONS );
 
+    const fractionLeftTextOptions = combineOptions<RichTextOptions>( {}, KeplersLawsConstants.TEXT_OPTIONS, {
+      maxWidth: 25
+    } );
     const fractionLeft = new FractionNode(
-        new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.symbols.periodStringProperty, model.selectedPeriodPowerProperty, new TinyProperty<boolean>( true ) ), KeplersLawsConstants.TEXT_OPTIONS ),
-        new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.symbols.semiMajorAxisStringProperty, model.selectedAxisPowerProperty, new TinyProperty<boolean>( true ) ), KeplersLawsConstants.TEXT_OPTIONS )
+      new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.symbols.periodStringProperty,
+        model.selectedPeriodPowerProperty, new TinyProperty<boolean>( true ) ), fractionLeftTextOptions ),
+      new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.symbols.semiMajorAxisStringProperty,
+        model.selectedAxisPowerProperty, new TinyProperty<boolean>( true ) ), fractionLeftTextOptions )
     );
 
     const fractionRight = new FractionNode(
-        new RichText( new DerivedProperty(
-          [ model.poweredPeriodProperty, model.engine.allowedOrbitProperty, KeplersLawsStrings.undefinedStringProperty ],
-          ( poweredPeriod, allowedOrbit, undefinedMessage ) => {
-            return allowedOrbit ? Utils.toFixed( poweredPeriod, 2 ) : undefinedMessage;
-          }
-        ), KeplersLawsConstants.TEXT_OPTIONS ),
-        new RichText( new DerivedProperty(
-          [ model.poweredSemiMajorAxisProperty, model.engine.allowedOrbitProperty, KeplersLawsStrings.undefinedStringProperty ],
-          ( poweredSemiMajorAxis, allowedOrbit, undefinedMessage ) => {
-            return allowedOrbit ? Utils.toFixed( poweredSemiMajorAxis, 2 ) : undefinedMessage;
-          }
-        ), KeplersLawsConstants.TEXT_OPTIONS )
+      new RichText( new DerivedProperty(
+        [ model.poweredPeriodProperty, model.engine.allowedOrbitProperty, KeplersLawsStrings.undefinedStringProperty ],
+        ( poweredPeriod, allowedOrbit, undefinedMessage ) => {
+          return allowedOrbit ? Utils.toFixed( poweredPeriod, 2 ) : undefinedMessage;
+        }
+      ), KeplersLawsConstants.TEXT_OPTIONS ),
+      new RichText( new DerivedProperty(
+        [ model.poweredSemiMajorAxisProperty, model.engine.allowedOrbitProperty, KeplersLawsStrings.undefinedStringProperty ],
+        ( poweredSemiMajorAxis, allowedOrbit, undefinedMessage ) => {
+          return allowedOrbit ? Utils.toFixed( poweredSemiMajorAxis, 2 ) : undefinedMessage;
+        }
+      ), KeplersLawsConstants.TEXT_OPTIONS )
     );
 
-    const unitsOptions = { font: new PhetFont( { size: 12 } ), fill: SolarSystemCommonColors.foregroundProperty };
     const createUnitsFraction = () => {
       return new FractionNode(
-          new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.units.yearsStringProperty, model.selectedPeriodPowerProperty, new TinyProperty<boolean>( true ) ), unitsOptions ),
-          new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.units.AUStringProperty, model.selectedAxisPowerProperty, new TinyProperty<boolean>( true ) ), unitsOptions ),
+        new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.units.yearsStringProperty, model.selectedPeriodPowerProperty, new TinyProperty<boolean>( true ) ), UNITS_TEXT_OPTIONS ),
+        new RichText( ThirdLawTextUtils.createPowerStringProperty( KeplersLawsStrings.units.AUStringProperty, model.selectedAxisPowerProperty, new TinyProperty<boolean>( true ) ), UNITS_TEXT_OPTIONS ),
         {
           visibleProperty: model.engine.allowedOrbitProperty
         }
