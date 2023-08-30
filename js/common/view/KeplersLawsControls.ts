514c6efc06db7f97d9cb2d5c8c1fd2f5a1bb86b2
pixelzoom <cmalley@pixelzoom.com>
2023-08-30 12:23:30 -0600

    reorganize to resolve layout problem, https://github.com/phetsims/keplers-laws/issues/148

--- js/common/view/KeplersLawsControls.ts
+++ js/common/view/KeplersLawsControls.ts
@@ -1,17 +1,13 @@
 // Copyright 2023, University of Colorado Boulder
 
-
 /**
- * Node that controls the extra visual elements of the sim. Designed as follows:
- * - The top part contains is a VBox, in it:
- *    - The target orbit selector.
- *    - The orbital information box. i.e. the checkboxes for each specific law
- * - The bottom part contains the checkboxes for the arrows and the information. Universal for all laws.
+ * KeplersLawsControls is the collection of panels that appears in the upper-right corner of all screens.
  *
  * @author Agustín Vallejo
+ * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import { HBox, HSeparator, Node, Text, VBox, VBoxOptions } from '../../../../scenery/js/imports.js';
+import { HBox, HSeparator, Node, Text, VBox } from '../../../../scenery/js/imports.js';
 import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import SolarSystemCommonConstants from '../../../../solar-system-common/js/SolarSystemCommonConstants.js';
@@ -28,67 +24,76 @@ import Stopwatch from '../../../../scenery-phet/js/Stopwatch.js';
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import TargetOrbitsPanel from './TargetOrbitsPanel.js';
 import GravityScaleSlider from '../../../../solar-system-common/js/view/GravityScaleSlider.js';
-import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
 
-class KeplersLawsControls extends VBox {
+export default class KeplersLawsControls extends VBox {
   public constructor( model: KeplersLawsModel, topLayer: Node, tandem: Tandem ) {
 
+    // Panel that contains the combo box for selecting a target orbit
     const targetOrbitsPanel = new TargetOrbitsPanel( model, topLayer );
 
-    // Creates a custom VBox with the provided and default options
-    const createVBox = ( children: Node[], providedOptions?: StrictOmit<VBoxOptions, 'children'> ) => {
-      return new VBox( combineOptions<VBoxOptions>( {
-        children: children,
-        spacing: 5,
-        align: 'left',
-        stretch: true
-      }, providedOptions ) );
-    };
+    // Panel that contains checkboxes related to Foci, Axes, and Eccentricity
+    const orbitalInformationPanel = new Panel( new KeplersLawsOrbitalInformationBox( model, {
+      tandem: Tandem.OPT_OUT
+    } ), combineOptions<PanelOptions>( {
+      // minWidth: SolarSystemCommonConstants.TEXT_MAX_WIDTH
+    }, SolarSystemCommonConstants.CONTROL_PANEL_OPTIONS ) );
+
+    // A bunch of unrelated controls, so we named the panel based on its position in the layout.
+    const bottomPanel = new Panel( new VBox( {
+      spacing: 5,
+      align: 'left',
+      stretch: true,
+      children: [
+
+        // 'Always circular' checkbox
+        new SolarSystemCommonCheckbox(
+          model.alwaysCircularProperty,
+          new Text( KeplersLawsStrings.circularOrbitStringProperty, KeplersLawsConstants.TEXT_OPTIONS ),
+          {
+            accessibleName: KeplersLawsStrings.circularOrbitStringProperty
+          } ),
+        new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
+
+        // 'Speed', 'Velocity', and 'Gravity Force' checkboxes
+        ...createArrowsVisibilityCheckboxes( model, Tandem.OPT_OUT ),
+
+        // Gravity slider
+        new GravityScaleSlider( model.forceScaleProperty, model.gravityVisibleProperty ),
+        new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
+
+        // 'Grid' and 'Measuring Tape' checkboxes
+        ...createVisibilityInformationCheckboxes( model, Tandem.OPT_OUT, false ),
+
+        // 'Stopwatch' checkbox
+        new SolarSystemCommonCheckbox(
+          model.stopwatchVisibleProperty,
+          new HBox( {
+            children: [
+              new Text( KeplersLawsStrings.stopwatchStringProperty, KeplersLawsConstants.TEXT_OPTIONS ),
+              createStopwatchIcon()
+            ]
+          } ),
+          {
+            accessibleName: KeplersLawsStrings.stopwatchStringProperty
+          } )
+      ]
+    } ), SolarSystemCommonConstants.CONTROL_PANEL_OPTIONS );
 
     super( {
       isDisposable: false,
+      spacing: 7,
       children: [
-        // Target orbit and individual laws checkboxes
-        createVBox( [
-          targetOrbitsPanel,
-          new Panel( new KeplersLawsOrbitalInformationBox( model, {
-            tandem: Tandem.OPT_OUT
-          } ), combineOptions<PanelOptions>( {
-            minWidth: SolarSystemCommonConstants.TEXT_MAX_WIDTH
-          }, SolarSystemCommonConstants.CONTROL_PANEL_OPTIONS ) )
-        ], { justify: 'top' } ),
-
-        // Common checkboxes
-        new Panel( createVBox( [
-          new SolarSystemCommonCheckbox(
-            model.alwaysCircularProperty,
-            new Text( KeplersLawsStrings.circularOrbitStringProperty, KeplersLawsConstants.TEXT_OPTIONS ),
-            {
-              accessibleName: KeplersLawsStrings.circularOrbitStringProperty
-            } ),
-          new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
-          ...createArrowsVisibilityCheckboxes( model, Tandem.OPT_OUT ),
-          new GravityScaleSlider( model.forceScaleProperty, model.gravityVisibleProperty ),
-          new HSeparator( SolarSystemCommonConstants.HSEPARATOR_OPTIONS ),
-          ...createVisibilityInformationCheckboxes( model, Tandem.OPT_OUT, false ),
-          new SolarSystemCommonCheckbox(
-            model.stopwatchVisibleProperty,
-            new HBox( {
-              children: [
-                new Text( KeplersLawsStrings.stopwatchStringProperty, KeplersLawsConstants.TEXT_OPTIONS ),
-                createStopwatchIcon()
-              ]
-            } ),
-            {
-              accessibleName: KeplersLawsStrings.stopwatchStringProperty
-            } )
-        ] ), SolarSystemCommonConstants.CONTROL_PANEL_OPTIONS )
-      ],
-      spacing: SolarSystemCommonConstants.CHECKBOX_SPACING
+        targetOrbitsPanel,
+        orbitalInformationPanel,
+        bottomPanel
+      ]
     } );
   }
 }
 
+/**
+ * Creates the icon for the stopwatch checkbox.
+ */
 function createStopwatchIcon(): Node {
   const stopwatchIcon = new StopwatchNode( new Stopwatch( {
     isVisible: true
@@ -109,4 +114,3 @@ function createStopwatchIcon(): Node {
 }
 
 keplersLaws.register( 'KeplersLawsControls', KeplersLawsControls );
-export default KeplersLawsControls;
