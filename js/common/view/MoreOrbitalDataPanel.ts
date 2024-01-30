8038d84b2f41342665b8e16ea1cca66491c829f7
phet-dev <phetadmin@gmail.com>
2024-01-30 03:15:00 -0700

    update copyright dates from daily grunt work

--- js/common/view/MoreOrbitalDataPanel.ts
+++ js/common/view/MoreOrbitalDataPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 /**
  * Panel that displays more data for the orbit, such as position and velocity vector values and directions.
  *
