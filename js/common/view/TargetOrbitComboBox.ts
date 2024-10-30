c9793e4028e502ec655007c0cc47c896cb8fca1e
phet-dev <phetadmin@gmail.com>
2024-10-30 03:17:09 -0600

    update copyright dates from daily grunt work

--- js/common/view/TargetOrbitComboBox.ts
+++ js/common/view/TargetOrbitComboBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * TargetOrbitComboBox is the combo box for selecting a target orbit.
--- js/common/view/ThirdLawAccordionBox.ts
+++ js/common/view/ThirdLawAccordionBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * ThirdLawAccordionBox contains the equation display and controls for the ThirdLawGraph class.
