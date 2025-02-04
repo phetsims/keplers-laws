33d68a70d874e15344d9fbf76dbdcbf7be3917f2
Jonathan Olson <jonathan.olson@colorado.edu>
2025-02-03 19:00:19 -0700

    Moving rasterized() and related methods to reduce circular references, see https://github.com/scenerystack/scenerystack/issues/8,  https://github.com/phetsims/chipper/issues/1550, https://github.com/phetsims/chipper/issues/871

--- js/common/view/KeplersLawsCheckbox.ts
+++ js/common/view/KeplersLawsCheckbox.ts
@@ -13,7 +13,7 @@ import { combineOptions, EmptySelfOptions } from '../../../../phet-core/js/optio
 import Stopwatch from '../../../../scenery-phet/js/Stopwatch.js';
 import StopwatchNode from '../../../../scenery-phet/js/StopwatchNode.js';
 import XNode from '../../../../scenery-phet/js/XNode.js';
-import { Image, Node } from '../../../../scenery/js/imports.js';
+import { Image, Node, rasterized } from '../../../../scenery/js/imports.js';
 import SolarSystemCommonCheckbox, { SolarSystemCommonCheckboxOptions } from '../../../../solar-system-common/js/view/SolarSystemCommonCheckbox.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import periodTimerIcon_png from '../../../images/periodTimerIcon_png.js';
@@ -196,7 +196,7 @@ function createStopwatchIcon(): Node {
     tandem: Tandem.OPT_OUT
   } );
 
-  const icon = stopwatchNode.rasterized( {
+  const icon = rasterized( stopwatchNode, {
     resolution: 5,
     nodeOptions: {
       cursor: 'pointer'
